e9685aa91d3290f993b185ed4244a36af96b8734
Phrytes <fritsderoos@gmail.com>
2020-03-27 12:14:12 +0100

    improved date retrieval

--- download_RKI-Covid19-data.py
+++ download_RKI-Covid19-data.py
@@ -4,6 +4,7 @@ import requests
 import re
 import os
 from bs4 import BeautifulSoup
+import datetime
 
 # You should use this source!
 # https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_Germany
@@ -12,9 +13,14 @@ RKI_url = 'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Fallzahl
 requestRKI = requests.get(RKI_url)
 soup = BeautifulSoup(requestRKI.content)
 table = soup.find("table")
-date = soup.find_all("div", class_="dateOfIssue")
-date = re.findall('\\d\\d\\.\\d\\d\\.\\d\\d\\d\\d', str(date))[0]
-date = date.replace(".", "-")
+allP = list(soup.find_all("p"))
+dateP = [p for p in allP if str(p).__contains__("online aktualisiert um")]
+# old way to find date
+# date = soup.find_all("div", class_="dateOfIssue")
+date = re.findall('\\d+', str(dateP))
+date = [int(part) for part in date]
+date = datetime.date(date[2], date[1], date[0])
+date = date.strftime("%d-%m-%Y")
 
 output_rows = []
 for table_row in table.findAll('tr'):
